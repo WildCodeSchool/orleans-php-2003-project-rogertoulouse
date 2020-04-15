314a062f5f4d049149435477bbf3f5adefb18a46
benoitchocot <benoit.chocot@gmail.com>
2020-04-15 15:39:01 +0200

    Add bg-color; redefine emplacements

--- src/routing.php
+++ src/routing.php
@@ -8,14 +8,14 @@
  *
  * @link     https://github.com/WildCodeSchool/simple-mvc
  */
-
 $routeParts = explode('/', ltrim($_SERVER['REQUEST_URI'], '/') ?: HOME_PAGE);
 $controller = 'App\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
 $method = $routeParts[1] ?? '';
 $vars = array_slice($routeParts, 2);
-
 if (class_exists($controller) && method_exists(new $controller(), $method)) {
     echo call_user_func_array([new $controller(), $method], $vars);
+} elseif (empty($method)) {
+    echo call_user_func_array([new $controller(), 'Index'], $vars);
 } else {
     header("HTTP/1.0 404 Not Found");
     echo '404 - Page not found';
