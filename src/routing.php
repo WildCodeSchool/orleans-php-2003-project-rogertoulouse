d2b63b4ceb83f50f5f65edc9529e78cdcda71350
Philippe NAVEILHAN <travail@naveilhan.com>
2020-04-15 15:47:49 +0200

    realise navbar in layout.html.twig with bootstrap

--- src/routing.php
+++ src/routing.php
@@ -16,6 +16,8 @@ $vars = array_slice($routeParts, 2);
 
 if (class_exists($controller) && method_exists(new $controller(), $method)) {
     echo call_user_func_array([new $controller(), $method], $vars);
+} elseif (empty($method)) {
+    echo call_user_func_array([new $controller(), 'Index'], $vars);
 } else {
     header("HTTP/1.0 404 Not Found");
     echo '404 - Page not found';
