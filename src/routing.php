7c772ed6921c54121f0b0fcbdb488b088254f67f
Julien <do.you.continue@gmail.com>
2020-04-16 15:20:31 +0200

    resolve Item

--- src/routing.php
+++ src/routing.php
@@ -11,7 +11,7 @@
 
 $routeParts = explode('/', ltrim($_SERVER['REQUEST_URI'], '/') ?: HOME_PAGE);
 $controller = 'App\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
-$method = $routeParts[1] ?? 'index';
+$method = $routeParts[1] ?? '';
 $vars = array_slice($routeParts, 2);
 
 if (class_exists($controller) && method_exists(new $controller(), $method)) {
