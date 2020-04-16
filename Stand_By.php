8f096b0703a2a4f561a292460f130e7fa8decacb
Philippe NAVEILHAN <travail@naveilhan.com>
2020-04-16 09:32:24 +0200

    remove pictures & search input, corrections PR

---
+++ Stand_By.php
@@ -0,0 +1,28 @@
+
+<--/ Navbar avec recherche /-->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="/home">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/biographie">Biographie</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/oeuvres">Oeuvres</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/association">Association</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">&#x1F50D;</button>
+        </form>
+    </div>
+</nav>
--- src/routing.php
+++ src/routing.php
@@ -11,13 +11,11 @@
 
 $routeParts = explode('/', ltrim($_SERVER['REQUEST_URI'], '/') ?: HOME_PAGE);
 $controller = 'App\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
-$method = $routeParts[1] ?? '';
+$method = $routeParts[1] ?? 'index';
 $vars = array_slice($routeParts, 2);
 
 if (class_exists($controller) && method_exists(new $controller(), $method)) {
     echo call_user_func_array([new $controller(), $method], $vars);
-} elseif (empty($method)) {
-    echo call_user_func_array([new $controller(), 'Index'], $vars);
 } else {
     header("HTTP/1.0 404 Not Found");
     echo '404 - Page not found';
