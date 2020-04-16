5293110792de840b3ec0d742d2e6e8c1b02b1a35
Julien <do.you.continue@gmail.com>
2020-04-16 14:28:34 +0200

    Finalisation Page Biographie

--- src/Controller/BiographieController.php
+++ src/Controller/BiographieController.php
@@ -27,90 +27,14 @@ class BiographieController extends AbstractController
      */
     public function index()
     {
-        $bioManager = new BiographieManager();
-        $biography = $bioManager->selectAllByDate();
+        $biographyManager = new BiographieManager();
+        $biographies = $biographyManager->selectAllByDate();
         $result = [];
-        foreach ($biography as $bio){
-            $date = explode('-', $bio['date']);
-            $result[] = $date[0];
+        foreach ($biographies as $biography) {
+            $biographyByYears[date_format(date_create($biography['date']), 'Y')][$biography['date']] = ['id' => $biography['id'], 'info' => $biography['info']];
         }
-        return $this->twig->render('Biography/index.html.twig', ['biography' => $biography, 'years' => array_unique($result)]);
-    }
-
-    /**
-     * Display item informations specified by $id
-     *
-     * @param int $id
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    public function show(int $id)
-    {
-        $bioManager = new BiographieManager();
-        $biography = $bioManager->selectOneById($id);
-
-        return $this->twig->render('Biography/show.html.twig', ['biography' => $biography]);
-    }
-
-    /**
-     * Display item edition page specified by $id
-     *
-     * @param int $id
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    public function edit(int $id): string
-    {
-        $bioManager = new BiographieManager();
-        $biography = $bioManager->selectOneById($id);
-        $btn = 'Editer';
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $biography['date'] = $_POST['date'];
-            $biography['info'] = $_POST['info'];
-            $bioManager->update($biography);
-        }
-
-        return $this->twig->render('Biography/edit.html.twig', ['biography' => $biography, 'btn' => $btn]);
-    }
-
-    /**
-     * Display item creation page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    public function add()
-    {
-        $btn = 'Ajouter';
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $bioManager = new BiographieManager();
-            $biography = [
-                'date' => $_POST['date'],
-                'info' => $_POST['info'],
-            ];
-            $bioManager->insert($biography);
-            header('Location:/Biographie/');
-        }
-
-        return $this->twig->render('Biography/add.html.twig', ['btn' => $btn]);
-    }
-
-    /**
-     * Handle item deletion
-     *
-     * @param int $id
-     */
-    public function delete(int $id)
-    {
-        $bioManager = new BiographieManager();
-        $bioManager->delete($id);
-        header('Location:/Biographie/');
+        return $this->twig->render('Biography/index.html.twig', [
+            'data' => $biographyByYears
+        ]);
     }
 }
--- src/Model/BiographieManager.php
+++ src/Model/BiographieManager.php
@@ -27,62 +27,6 @@ class BiographieManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
-
-    /**
-     * @param array $biography
-     * @return int
-     */
-    public function insert(array $biography): int
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE .
-            " (`date`, `info`) VALUES (:date, :info)");
-        $statement->bindValue('date', $biography['date'], \PDO::PARAM_STR);
-        $statement->bindValue('info', $biography['info'], \PDO::PARAM_STR);
-        if ($statement->execute()) {
-            return (int)$this->pdo->lastInsertId();
-        }
-    }
-
-
-    /**
-     * @param int $id
-     */
-    public function delete(int $id): void
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . " WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-    }
-
-
-    /**
-     * @param array $biography
-     * @return bool
-     */
-    public function update(array $biography):bool
-    {
-
-        // prepared request
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE .
-            " SET `date` = :date, `info` = :info WHERE id=:id");
-        $statement->bindValue('id', $biography['id'], \PDO::PARAM_INT);
-        $statement->bindValue('date', $biography['date'], \PDO::PARAM_STR);
-        $statement->bindValue('info', $biography['info'], \PDO::PARAM_STR);
-
-        return $statement->execute();
-    }
-
-    /**
-     * Get all row from database.
-     *
-     * @return array
-     */
-    public function selectAllDate(): array
-    {
-        return $this->pdo->query('SELECT DISTINCT YEAR(`date`) AS date FROM ' . $this->table)->fetchAll();
-    }
     /**
      * Get all row from database.
      *
