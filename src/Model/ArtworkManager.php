1b8c3f2c8f77a0593011d7e91b2a2181baaff856
Philippe NAVEILHAN <travail@naveilhan.com>
2020-04-22 21:46:52 +0200

    mise en place des éléments & possibilités de tri par catégorie & chronologique

--- src/Model/ArtworkManager.php
+++ src/Model/ArtworkManager.php
@@ -11,10 +11,34 @@ class ArtworkManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
+    public function selectAllArtworks()
+    {
+        $result = $this->pdo->query('SELECT * FROM '.$this->table.' a 
+        JOIN works_category c ON a.category_id=c.ID')->fetchAll();
+        shuffle($result);
+        return $result;
+    }
+
     public function selectAllByCategory()
     {
         $listArtwork = $this->pdo->query('SELECT * FROM '.$this->table.' a 
         JOIN works_category c ON a.category_id=c.ID')->fetchAll();
+
+        return $listArtwork;
+    }
+
+    public function selectAllArtworksByCat($cat)
+    {
+        $result = $this->pdo->query('SELECT * FROM '.$this->table.' a 
+        JOIN works_category c ON a.category_id=c.ID WHERE c.ID='.$cat.'')->fetchAll();
+        shuffle($result);
+        return $result;
+    }
+
+    public function selectAllByOrder($direction)
+    {
+        $listArtwork = $this->pdo->query('SELECT * FROM '.$this->table.' a 
+        JOIN works_category c ON a.category_id=c.ID ORDER BY a.date '.$direction.'')->fetchAll();
 
         return $listArtwork;
     }
