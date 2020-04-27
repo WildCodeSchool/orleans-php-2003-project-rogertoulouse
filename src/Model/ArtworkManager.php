776a49d94f1e3a7ac205c7b01ec13c34654244df
Philippe NAVEILHAN <travail@naveilhan.com>
2020-04-27 14:20:28 +0200

    adapt BiographyController à la nouvelle methode de ArtworkManager

--- src/Model/ArtworkManager.php
+++ src/Model/ArtworkManager.php
@@ -16,7 +16,6 @@ class ArtworkManager extends AbstractManager
         $statement='SELECT * FROM ' . $this->table . ' a JOIN works_category c 
         ON a.category_id=c.id' . ($categoryArtwork !=null ? ' WHERE c.id=' . $categoryArtwork : '' );
         $artworks = $this->pdo->query($statement)->fetchAll();
-        shuffle($artworks);
         return $artworks;
     }
 
