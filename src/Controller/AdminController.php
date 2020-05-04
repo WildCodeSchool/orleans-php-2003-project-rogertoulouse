cfa046a24e11b7186c93c77123a461cce8f461a8
benoitchocot <benoit.chocot@gmail.com>
2020-05-04 16:16:21 +0200

    crud part 1 done

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -31,40 +31,104 @@ class AdminController extends AbstractController
     {
         return $this->twig->render('/Admin/index.html.twig');
     }
+
     public function home()
     {
-        return $this->twig->render('/Admin/Home/home.html.twig', ['active' => 'home']);
+        $newsManager = new NewsManager();
+        $news = $newsManager->selectNews();
+
+        return $this->twig->render('Admin/Home/index.html.twig', ['news' => $news]);
+    }
+
+
+    /**
+     * Display item informations specified by $id
+     *
+     * @param int $id
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function show(int $id)
+    {
+        $newManager = new NewsManager();
+        $new = $newManager->selectOneById($id);
+
+        return $this->twig->render('Admin/Home/show.html.twig', ['new' => $new]);
+    }
+
+
+    /**
+     * Display item edition page specified by $id
+     *
+     * @param int $id
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function update(int $id): string
+    {
+        $newManager = new NewsManager();
+        $new = $newManager->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $new['title'] = $_POST['title'];
+            $newManager->update($new);
+        }
+
+        return $this->twig->render('Admin/Home/edit.html.twig', ['new' => $new]);
+    }
+
+
+    /**
+     * Display item creation page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function add()
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $newManager = new newsManager();
+            $new = [
+                'title' => $_POST['title'],
+            ];
+            $id = $newManager->insert($new);
+            header('Location:/Admin/Home/show/' . $id);
+        }
+
+        return $this->twig->render('Admin/Home/add.html.twig');
+    }
+
+
+    /**
+     * Handle item deletion
+     *
+     * @param int $id
+     */
+    public function delete(int $id)
+    {
+        $newManager = new NewsManager();
+        $newManager->delete($id);
+        header('Location:/Admin/Home/index');
     }
     public function biography()
     {
         return $this->twig->render('/Admin/Biography/biography.html.twig', ['active' => 'biography']);
     }
+
     public function artworks()
     {
         return $this->twig->render('/Admin/Artworks/artworks.html.twig', ['active' => 'artworks']);
     }
+
     public function association()
     {
         return $this->twig->render('/Admin/Association/association.html.twig', ['active' => 'association']);
-    }
-
-    public function edit($id): string
-    {
-        $newsManager = new NewsManager();
-        $news = $newsManager->selectOneById($id);
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $news['title'] = $_POST['title'];
-            $newsManager->update($news);
-        }
-
-        return $this->twig->render('admin/home/edit.html.twig', ['news' => $news]);
-    }
-    public function show($id): int
-    {
-        $newsManager = new NewsManager();
-        $news = $newsManager->selectOneById($id);
-
-        return $this->twig->render('admin/home/show.html.twig', ['news' => $news]);
     }
 }
--- src/Model/NewsManager.php
+++ src/Model/NewsManager.php
@@ -1,28 +1,78 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
 
 namespace App\Model;
 
+/**
+ *
+ */
 class NewsManager extends AbstractManager
 {
+    /**
+     *
+     */
     const TABLE = 'news';
 
+    /**
+     *  Initializes this class.
+     */
     public function __construct()
     {
         parent::__construct(self::TABLE);
     }
-    public function selectNews(): array
+
+
+    /**
+     * @param array $news
+     * @return int
+     */
+    public function insert(array $news): int
     {
-        return $this->pdo->query('SELECT * FROM ' . $this->table)->fetchAll();
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`title`) VALUES (:title)");
+        $statement->bindValue('title', $news['title'], \PDO::PARAM_STR);
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
     }
+
+
+    /**
+     * @param int $id
+     */
+    public function delete(int $id): void
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . " WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+
+    /**
+     * @param array $news
+     * @return bool
+     */
     public function update(array $news):bool
     {
+
         // prepared request
         $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
         $statement->bindValue('id', $news['id'], \PDO::PARAM_INT);
         $statement->bindValue('title', $news['title'], \PDO::PARAM_STR);
-        $statement->bindValue('button', $news['button'], \PDO::PARAM_STR);
-        $statement->bindValue('button_link', $news['button_link'], \PDO::PARAM_STR);
 
         return $statement->execute();
+    }
+
+    public function selectNews(): array
+    {
+        return $this->pdo->query('SELECT * FROM ' . $this->table)->fetchAll();
     }
 }
