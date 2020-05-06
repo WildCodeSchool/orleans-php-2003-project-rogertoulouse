65ab4780cd5eae8bd52f102a29876f207ac438ac
Tobe_Continued <do.you.continue@gmail.com>
2020-05-06 13:19:58 +0200

    resolve sylvain need

--- src/Controller/BiographyAdminController.php
+++ src/Controller/BiographyAdminController.php
@@ -24,7 +24,6 @@ use Twig\Error\SyntaxError;
 class BiographyAdminController extends AbstractController
 {
     protected $active = 'biography';
-    protected $dataByYears = [];
 
     /**
      * Display home page
@@ -37,10 +36,10 @@ class BiographyAdminController extends AbstractController
      */
     public function index()
     {
-        $this->populateData();
+        $dataByYears = $this->populateData();
         return $this->twig->render('/BiographyAdmin/index.html.twig', [
             'active' => $this->active,
-            'data' => $this->dataByYears]);
+            'data' => $dataByYears]);
     }
 
 
@@ -52,22 +51,28 @@ class BiographyAdminController extends AbstractController
      */
     public function add()
     {
+        $dataByYears = $this->populateData();
         $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $data = array_map('trim', $_POST);
             if (isset($data['year'])) {
-                $year = htmlentities($data['year']);
-                $startDate = strtotime('01-01-1916');
-                $nowDate = strtotime(date('m-d-Y'));
-                $userDate = strtotime('01-01-' . $year);
-                if ((strtotime($year)) === false) {
-                    $errors['year'][] = 'L\'année n\'est pas valide.';
-                } elseif ($userDate <= $startDate || $userDate >= $nowDate) {
-                    $errors['year'][] = 'L\'année doit être comprise entre ' .
-                        date('Y', $startDate) .
-                        ' et ' .
-                        date('Y', $nowDate) .
-                        '.';
+                if ($data['year'] != '') {
+                    $year = $data['year'];
+                    $userDate = '01-01-' . $year;
+                    $startDate = new DateTime('01-01-1916');
+                    $nowDate = new DateTime(date('m-d-Y'));
+                    $userDate = new DateTime($userDate);
+                    if ((strtotime($year)) === false) {
+                        $errors['year'][] = 'L\'année n\'est pas valide.';
+                    } elseif ($userDate <= $startDate || $userDate >= $nowDate) {
+                        $errors['year'][] = 'L\'année doit être comprise entre ' .
+                            $startDate->format('Y') .
+                            ' et ' .
+                            $nowDate->format('Y') .
+                            '.';
+                    }
+                } else {
+                    $errors['year'][] = 'L\'année ne doit pas être vide.';
                 }
             } else {
                 $errors['year'][] = 'L\'année ne doit pas être vide.';
@@ -76,33 +81,33 @@ class BiographyAdminController extends AbstractController
                 $errors['biography'][] = 'La biographie ne doit pas être vide.';
             }
             if (!empty($errors)) {
-                $this->populateData();
                 return $this->twig->render('/BiographyAdmin/_add.html.twig', [
                     'active' => $this->active,
-                    'data' => $this->dataByYears,
+                    'data' => $dataByYears,
                     'errors' => $errors]);
             }
             $biographyManager = new BiographyManager();
             $date = new DateTime($data['year'] . '-01-01');
-            $biographyManager->insert($date->format('Y-m-d'), htmlentities($data['biography']));
+            $biographyManager->insert($date->format('Y-m-d'), $data['biography']);
             header('Location:/BiographyAdmin/Index');
         }
-        $this->populateData();
         return $this->twig->render('/BiographyAdmin/_add.html.twig', [
             'active' => $this->active,
-            'data' => $this->dataByYears,
+            'data' => $dataByYears,
             'errors' => $errors]);
     }
     private function populateData()
     {
+        $data = [];
         $biographyManager = new BiographyManager();
         $dataByDates[] = $biographyManager->selectAllBioByDate();
         foreach ($dataByDates as $biographies) {
             foreach ($biographies as $biography) {
                 $year = date_format(date_create($biography['date']), 'Y');
-                $this->dataByYears[$year][] = $biography;
+                $data[$year][] = $biography;
             }
         }
-        ksort($this->dataByYears);
+        ksort($data);
+        return $data;
     }
 }
