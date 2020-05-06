8c3c11f8e97ae9eb347561c4fd6c3a721c04fbaf
benoitchocot <benoit.chocot@gmail.com>
2020-05-06 12:19:24 +0200

    reste l'index avec l'affichage des données bdd

--- src/Model/NewsManager.php
+++ src/Model/NewsManager.php
@@ -32,57 +32,6 @@ class NewsManager extends AbstractManager
      * @param array $news
      * @return int
      */
-    public function insert(array $news): int
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`title`,`desc`,`button`,
-        `button_link`) VALUES (:title, :desc, :button, :button_link)");
-        $statement->bindValue('title', $news['title'], \PDO::PARAM_STR);
-        $statement->bindValue('desc', $news['desc'], \PDO::PARAM_STR);
-        $statement->bindValue('button', $news['button'], \PDO::PARAM_STR);
-        $statement->bindValue('button_link', $news['button_link'], \PDO::PARAM_STR);
-
-        if ($statement->execute()) {
-            return (int)$this->pdo->lastInsertId();
-        }
-    }
-
-
-    /**
-     * @param int $id
-     */
-    public function delete(int $id)
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . " WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-    }
-    public function deleteRoom()
-    {
-        $roomManager = new RoomManager();
-        $id = trim($_POST['id']);
-        $roomManager->deleteRoom($id);
-        header('Location: ../AdminRoom/index');
-    }
-
-    /**
-     * @param array $news
-     * @return bool
-     */
-    public function update(array $news):bool
-    {
-
-        // prepared request
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
-        $statement->bindValue('id', $news['id'], \PDO::PARAM_INT);
-        $statement->bindValue('title', $news['title'], \PDO::PARAM_STR);
-        $statement->bindValue('desc', $news['desc'], \PDO::PARAM_STR);
-        $statement->bindValue('button', $news['button'], \PDO::PARAM_STR);
-        $statement->bindValue('button_link', $news['button_link'], \PDO::PARAM_STR);
-
-        return $statement->execute();
-    }
 
     public function selectNews(): array
     {
